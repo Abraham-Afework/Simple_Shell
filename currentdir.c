0181382dc25e9d2d385da46e15f011c7b988aedd
Abraham Afework <Abrahamafework7@gmail.com>
2022-12-21 20:04:38 +0200

    currentdir function added after betty check

---
+++ currentdir.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * currentdir - function that prints current directory
+ *
+ */
+
+void currentdir(void)
+{
+	char buff[PATH_MAX];
+
+	/*	printf("%s",getcwd(buff,PATH_MAX));*/
+	printf("$");
+}
